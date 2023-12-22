45bc0b0f710ddc80de997ea20ad00ee5aaff9172
Brunovncs <brunoviniciusrp@gmail.com>
2023-12-22 13:07:13 -0300

    pong-pythongame

---
+++ pong.py
@@ -0,0 +1,38 @@
+import turtle
+
+win = turtle.Screen()
+win.title("Pong")
+win.bgcolor("#222f3e")
+win.setup(width=800, height=600)
+win.tracer(0)
+
+# Paddle A
+paddle_a = turtle.Turtle()
+paddle_a.speed(0)
+paddle_a.shape("square")
+paddle_a.color("white")
+paddle_a.shapesize(stretch_wid=5, stretch_len=0.5)
+paddle_a.penup()
+paddle_a.goto(-350, 0)
+
+# Paddle B
+paddle_b = turtle.Turtle()
+paddle_b.speed(0)
+paddle_b.shape("square")
+paddle_b.color("white")
+paddle_b.shapesize(stretch_wid=5, stretch_len=0.5)
+paddle_b.penup()
+paddle_b.goto(350, 0)
+
+# Ball
+
+ball = turtle.Turtle()
+ball.speed(0)
+ball.shape("circle")
+ball.color("white")
+ball.penup()
+ball.goto(0, 0)
+
+# Main game loop
+while True:
+    win.update()
